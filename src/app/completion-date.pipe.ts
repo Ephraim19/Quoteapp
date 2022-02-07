60a32caf51535ed52e25980a00c76b6d357a5db3
Ephraim19 <machayoephraim@gmail.com>
2022-02-07 06:46:08 +0300

    Added code to tell the number of days since the quote was creted

--- src/app/completion-date.pipe.ts
+++ src/app/completion-date.pipe.ts
@@ -1,21 +1,25 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'completionDate'
+  name: 'completionDate',
 })
 export class CompletionDatePipe implements PipeTransform {
-
   transform(value: any): number {
-    let today:Date = new Date()
-    let date:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
-    var dateDifference =  value - date
-    var dateDifferenceSeconds = dateDifference*0.001; 
-    var dateCounter = dateDifferenceSeconds/86400;
-    if (dateCounter >= 1 && value > date){
-      return dateCounter -27;
-    }else{
+    let today: Date = new Date();
+    let date: any = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate()
+    );
+
+    var difference = date.getTime() - value.getTime();
+    var days = difference / (1000 * 3600 * 24);
+    //var dateDifferenceSeconds = dateDifference * 0.001;
+    //var dateCounter = dateDifferenceSeconds / 86400;
+    if (days > 0) {
+      return days;
+    } else {
       return 0;
     }
   }
-
 }
